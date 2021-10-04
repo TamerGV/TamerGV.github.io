80d4ed3f131d00422a5babb2a5aecd4314577a82
TamerGV <87567326+TamerGV@users.noreply.github.com>
2021-10-04 20:15:43 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-const light = document.querySelector('.light'),
-      night = document.querySelector('.night'),
-      img = document.querySelector('img');
+$(document).ready(function() {
+    $('#menu').click(function() {
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+    });
 
-light.addEventListener('click', function() {
-    img.setAttribute('src', 'https://st.depositphotos.com/1310390/1389/v/600/depositphotos_13897533-stock-illustration-lit-light-bulb.jpg');
-});
-
-night.addEventListener('click', function() {
-    img.setAttribute('src', 'https://s.felomena.com/wp-content/images/sonnik/bukva/l/lampochka.jpg');
+    $(window).on('scroll load', function() {
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
+    });
 });
 
